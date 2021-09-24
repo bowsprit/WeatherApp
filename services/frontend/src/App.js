51ff4d92c0767f85ab7c5b5f040a6c0ad180459f
Tyler Haas <tyler.haas@getcruise.com>
2021-09-24 16:23:34 -0700

    Formatting

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -3,37 +3,35 @@ import SearchBar from "./components/SearchBar";
 import { useState } from "react";
 
 export const App = () => {
-
   const defaultCity = {
-      city: "San Francisco",
-      state: "CA"
-  }
-  
-  const [mainCity, setMainCity] = useState(defaultCity)
+    city: "San Francisco",
+    state: "CA",
+  };
+
+  const [mainCity, setMainCity] = useState(defaultCity);
   const [suggestedCities, setSuggestedCities] = useState([
-      {city: "Chicago", state: "IL"},
-      {city: "Oakland", state: "CA"},
-      {city: "New York", state: "NY"},
-  ])
+    { city: "Chicago", state: "IL" },
+    { city: "Oakland", state: "CA" },
+    { city: "New York", state: "NY" },
+  ]);
 
   const updateMainCity = (newLocation) => {
-      if (newLocation !== mainCity) {
-          // Prepend prev. mainCity to top of suggested cities array
-          setSuggestedCities([mainCity, ...suggestedCities]);
-      }
-      // React is smart enough to determine if the new content differs from the old.
-      // So we can do this outside the if statement.
-      setMainCity({city: newLocation.city, state: newLocation.state});
-  }
+    if (newLocation !== mainCity) {
+      // Prepend prev. mainCity to top of suggested cities array
+      setSuggestedCities([mainCity, ...suggestedCities]);
+    }
+    // React is smart enough to determine if the new content differs from the old.
+    // So we can do this outside the if statement.
+    setMainCity({ city: newLocation.city, state: newLocation.state });
+  };
 
   return (
     <div className="flex flex-col">
-      <SearchBar handleUserSearch={updateMainCity}/>
-      <WeatherForecast
-        locationData={mainCity}
-        defaultExpanded={true}
-      />
-      {suggestedCities.slice(0, 3).map((suggestedCity, index) => {return <WeatherForecast locationData={suggestedCity} key={index}/>})}
+      <SearchBar handleUserSearch={updateMainCity} />
+      <WeatherForecast locationData={mainCity} defaultExpanded={true} />
+      {suggestedCities.slice(0, 3).map((suggestedCity, index) => {
+        return <WeatherForecast locationData={suggestedCity} key={index} />;
+      })}
     </div>
   );
 };
--- services/frontend/src/components/SearchBar.js
+++ services/frontend/src/components/SearchBar.js
@@ -1,38 +1,39 @@
 import { useState } from "react";
-import { PropTypes } from 'prop-types';
+import { PropTypes } from "prop-types";
 
-const SearchBar = ({handleUserSearch}) => {
-
+const SearchBar = ({ handleUserSearch }) => {
   const [userInput, setUserInput] = useState("");
 
   const handleUserInputChange = (event) => {
-      setUserInput(event.target.value);
+    setUserInput(event.target.value);
   };
 
   const handleFormSubmit = (event) => {
-      event.preventDefault();
-      try {
-          const parsedLocation = validateFormData(userInput);
-          handleUserSearch(parsedLocation);
-      } catch (e) {
-          alert("Search input could not be pased. Please use `City, State` format.");
-      }
-      setUserInput('');
+    event.preventDefault();
+    try {
+      const parsedLocation = validateFormData(userInput);
+      handleUserSearch(parsedLocation);
+    } catch (e) {
+      alert(
+        "Search input could not be pased. Please use `City, State` format."
+      );
+    }
+    setUserInput("");
   };
 
   const validateFormData = (stringData) => {
-      // This function will be replaced by backend parsing
-      let splitString = stringData.split(",");
-      console.log(splitString);
-      if (splitString.length !== 2) {
-          throw new Error(`Unable to parse input ${stringData}`);
-      }
-      const location = {
-          city: splitString[0].trim(),
-          state: splitString[1].trim(),
-      };
-      return location;
-  }
+    // This function will be replaced by backend parsing
+    let splitString = stringData.split(",");
+    console.log(splitString);
+    if (splitString.length !== 2) {
+      throw new Error(`Unable to parse input ${stringData}`);
+    }
+    const location = {
+      city: splitString[0].trim(),
+      state: splitString[1].trim(),
+    };
+    return location;
+  };
 
   return (
     <div className="md:w-1/2 mx-auto my-5 rounded-xl shadow-md text-gray-700 text-center text-xl p-4 bg-gray-200">
@@ -53,7 +54,7 @@ const SearchBar = ({handleUserSearch}) => {
 };
 
 SearchBar.propTypes = {
-    handleUserSearch: PropTypes.func,
+  handleUserSearch: PropTypes.func,
 };
 
 export default SearchBar;
