26d71ce5118b78ac936370ede5680addb4d55301
Tyler Haas <tyler.haas@getcruise.com>
2021-09-24 16:14:35 -0700

    Add search bar handling, refac App to be stateful

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,16 +1,39 @@
 import WeatherForecast from "./components/WeatherForecast";
 import SearchBar from "./components/SearchBar";
+import { useState } from "react";
 
 export const App = () => {
+
+  const defaultCity = {
+      city: "San Francisco",
+      state: "CA"
+  }
+  
+  const [mainCity, setMainCity] = useState(defaultCity)
+  const [suggestedCities, setSuggestedCities] = useState([
+      {city: "Chicago", state: "IL"},
+      {city: "Oakland", state: "CA"},
+      {city: "New York", state: "NY"},
+  ])
+
+  const updateMainCity = (newLocation) => {
+      if (newLocation !== mainCity) {
+          // Prepend prev. mainCity to top of suggested cities array
+          setSuggestedCities([mainCity, ...suggestedCities]);
+      }
+      // React is smart enough to determine if the new content differs from the old.
+      // So we can do this outside the if statement.
+      setMainCity({city: newLocation.city, state: newLocation.state});
+  }
+
   return (
     <div className="flex flex-col">
-      <SearchBar />
+      <SearchBar handleUserSearch={updateMainCity}/>
       <WeatherForecast
-        locationData={{ city: "San Francisco", state: "CA" }}
+        locationData={mainCity}
         defaultExpanded={true}
       />
-      <WeatherForecast locationData={{ city: "Chicago", state: "IL" }} />
-      <WeatherForecast locationData={{ city: "Oakland", state: "CA" }} />
+      {suggestedCities.slice(0, 3).map((suggestedCity, index) => {return <WeatherForecast locationData={suggestedCity} key={index}/>})}
     </div>
   );
 };
